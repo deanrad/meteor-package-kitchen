6e5ea6fb48e95038945816fa9978665b498eb892
Dean Radcliffe <dradcliffe@opinionlab.com>
2015-04-10 11:41:07 -0500

    1.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
  name: "deanius:package-kitchen",
  summary: "Source for http://package-kitchen.meteor.com - not for installation",
- version: "1.0.0",
+ version: "1.0.1",
  git: "https://github.com/chicagogrooves/meteor-package-kitchen",
  debugOnly: true
 });
