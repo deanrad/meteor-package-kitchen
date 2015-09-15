b3ed50a846b7aa8cc4e16fe75d99bd5d14bce82a
Dean Radcliffe <deanmisc@yahoo.com>
2015-09-15 12:31:43 -0500

    can delete deps

--- client/helpers.js
+++ client/helpers.js
@@ -11,6 +11,15 @@ Template['package-kitchen-kitchen'].events({
   }
 });
 
+Template['package-kitchen-editor'].events({
+  'click .deleteDep': function (e, t) {
+    var packageName  = e.target.attributes['data-package-name'].value;
+    var packages = packageViewModel.packageDeps();
+    var delIdx = packages.indexOf(packageName);
+    if(delIdx > -1) packages.splice(delIdx, 1);
+  }
+})
+
 Template['package-kitchen-editor'].helpers({
   packageDeps: function () {
     //this helper will be called at least once before our VM, or its global variable are defined
@@ -21,8 +30,10 @@ Template['package-kitchen-editor'].helpers({
 Template['package-kitchen-editor'].onRendered( function () {
   $("#addNewPackageDep").on('submit', function (e) {
     e.preventDefault();
-    var packageName = $("input[name=addNewPackageDep]").val();
+    var $pkg = $("input[name=addNewPackageDep]");
+    var packageName = $pkg.val();
     packageViewModel.packageDeps().push(packageName);
+    $pkg.val('');
   });
 });
 
