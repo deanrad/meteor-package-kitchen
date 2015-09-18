d42a68f5f8300b87b4eabfdac3035fd3f04e858b
Dean Radcliffe <deanmisc@yahoo.com>
2015-09-18 10:07:04 -0500

    Full workaround for download blockage
    Works even better now!

--- client/zip.js
+++ client/zip.js
@@ -9,12 +9,16 @@ zipPackage = function () {
   var base64contents = zip.generate({type:"base64"});
 
   var packageDirName = packageViewModel.fullPackageName().replace(":", "\\:");
-  $(".allFiles").append($("<textarea/>").val(
+  $(".allFiles")
+    .append($("<div>").text("Paste the following into your terminal:"))
+    .append($("<textarea rows='10' style='width: 100%'/>").val(
     "echo -n " + base64contents + " | pbcopy;\n" +
     "mkdir -p packages/" + packageDirName + ";\n" +
     "cd packages/" + packageDirName + ";\n" +
     "pbpaste | base64 -D > tmp.zip;\n" +
-    "unzip tmp.zip; rm tmp.zip; cd ../..; \n"
+    "unzip tmp.zip; rm tmp.zip; cd ../..; \n" +
+    "meteor add " + packageViewModel.fullPackageName() + "\n"
   ));
+  $(window).scrollTop(2000);
   //window.location = "data:application/zip;base64," + base64contents;
 }
