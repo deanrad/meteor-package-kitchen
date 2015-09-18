122739345f001370d4aec09790fd2bb060e0daf9
Dean Radcliffe <deanmisc@yahoo.com>
2015-09-17 21:18:32 -0500

    workaround for chrome WIP

--- client/zip.js
+++ client/zip.js
@@ -2,9 +2,19 @@ zipPackage = function () {
   console.log("zipping package");
   var zipContent, zip = new JSZip();
 
-  ViewModel.byId("packageModel").allFilesRendered().forEach(function (file) {
+  packageViewModel.allFilesRendered().forEach(function (file) {
     zip.file(file.path, file.contents);
   });
 
-  window.location = "data:application/zip;base64," + zip.generate({type:"base64"});
+  var base64contents = zip.generate({type:"base64"});
+
+  var packageDirName = packageViewModel.fullPackageName().replace(":", "\\:");
+  $(".allFiles").append($("<textarea/>").val(
+    "echo -n " + base64contents + " | pbcopy;\n" +
+    "mkdir -p packages/" + packageDirName + ";\n" +
+    "cd packages/" + packageDirName + ";\n" +
+    "pbpaste | base64 -D > tmp.zip;\n" +
+    "unzip tmp.zip; rm tmp.zip; cd ../..; \n"
+  ));
+  //window.location = "data:application/zip;base64," + base64contents;
 }
